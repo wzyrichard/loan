b0ddf58bb3648a904590a3835b50545d21ba264f
Yan <1031685990@qq.com>
2019-07-03 20:20:39 +0800

    建好所需数据库表格

--- src/main/resources/sql/db_loan.sql
+++ src/main/resources/sql/db_loan.sql
@@ -1,19 +1,75 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : LocalMySQL
-Source Server Version : 50725
-Source Host           : 127.0.0.1:3306
+Source Server         : aa
+Source Server Version : 80015
+Source Host           : localhost:3306
 Source Database       : db_loan
 
 Target Server Type    : MYSQL
-Target Server Version : 50725
+Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-06-28 09:07:20
+Date: 2019-07-03 20:18:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_asset
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_asset`;
+CREATE TABLE `tb_asset` (
+  `assetId` bigint(11) NOT NULL COMMENT '财务状况主键',
+  `asset_uid` bigint(11) DEFAULT NULL COMMENT '财务状况用户id',
+  `finance` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '金融类资产',
+  `finance_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '金融类资产明细',
+  `creature` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '生物资产',
+  `creature_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '生物资产明细',
+  `stock` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '存货',
+  `stock_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '存货明细',
+  `recievable` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '应收款',
+  `recievable_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '应收款明细',
+  `other_current_asset` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '' COMMENT '其他流动资产',
+  `other_current_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他流动资产明细',
+  `current_asset_total` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '流动资产合计',
+  `current_total_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '流动资产合计明细',
+  `house` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '房产',
+  `house_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '房产明细',
+  `service` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '农机设备车辆',
+  `service_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '农机设备车辆明细',
+  `run_right` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '经营使用权',
+  `run_right_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '经营使用权明细',
+  `other_fixed_asset` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他非流动资产',
+  `other_fixed_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他非流动资产明细',
+  `fixed_asset_total` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '非流动资产合计',
+  `fixed_total_detial` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '非流动资产合计明细',
+  `asset_amount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '总合计',
+  `asset_amount_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '总合计明细',
+  `bank_loan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '银行借款',
+  `bank_loan_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '银行借款明细',
+  `credit_loan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '信用社借款',
+  `credit_loan_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '信用社借款明细',
+  `retailbank_loan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他金融机构借款',
+  `retailbank_loan_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他金融机构借款明细',
+  `folk_loan` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '民间贷款',
+  `folk_loan_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '民间贷款明细',
+  `other_debt` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他负债',
+  `other_debt_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '其他负债明细',
+  `debt_amount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '负债合计',
+  `debt_amount_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '负债合计明细',
+  `repayment_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '年还款额',
+  `repayment_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '年还款额明细',
+  `net_asset` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '净资产',
+  `net_asset_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '净资产明细',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`assetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of tb_asset
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for tb_business_info
@@ -21,6 +77,25 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `tb_business_info`;
 CREATE TABLE `tb_business_info` (
   `bizId` bigint(12) NOT NULL AUTO_INCREMENT COMMENT '营业信息主键',
+  `business_uid` bigint(225) DEFAULT NULL COMMENT '经营信息用户id',
+  `business_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '经营项目名称',
+  `scale` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '经营规模',
+  `years` int(255) DEFAULT NULL COMMENT '已经营年限',
+  `field` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '场所性质',
+  `land_area` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '场所规模',
+  `payment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '第一年投入',
+  `self_money` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '自筹资金',
+  `lend` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '贷款资金',
+  `lend_rate` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '贷款总投入占比',
+  `income` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '前一年销售收入',
+  `net_income` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '前一年净收入',
+  `prospective_yeild` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '来年预期收入',
+  `pro_self_money` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '预期自筹资金',
+  `pro_lend` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '预期贷款资金',
+  `prospective_income` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '预期销售收入',
+  `prospective_net_income` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '预期净收入',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`bizId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -34,6 +109,16 @@ CREATE TABLE `tb_business_info` (
 DROP TABLE IF EXISTS `tb_loan`;
 CREATE TABLE `tb_loan` (
   `loanId` bigint(12) NOT NULL AUTO_INCREMENT COMMENT '贷款主键',
+  `loan_uid` bigint(12) DEFAULT NULL COMMENT '贷款用户id',
+  `loan_amount` varchar(10) DEFAULT NULL,
+  `loan_term` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '贷款期限',
+  `guarantee_method` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '担保方式',
+  `guarantor_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '担保人名称',
+  `loan_method` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '贷款方式',
+  `repayment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '还款方式',
+  `loan_use` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '贷款用途',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`loanId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -47,6 +132,14 @@ CREATE TABLE `tb_loan` (
 DROP TABLE IF EXISTS `tb_material`;
 CREATE TABLE `tb_material` (
   `mdId` bigint(12) NOT NULL AUTO_INCREMENT COMMENT '资料主键',
+  `mdld_uid` bigint(12) DEFAULT NULL COMMENT '资料用户id',
+  `hus_idcard` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '丈夫身份证',
+  `wife_idcard` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '妻子身份证',
+  `residence` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '户口簿',
+  `marriage_lines` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '结婚证',
+  `land_deed` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '土地证',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`mdId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -73,7 +166,7 @@ CREATE TABLE `tb_user` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_user
